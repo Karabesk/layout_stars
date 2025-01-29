59161f726cecf1b25227650bd621936c84880c5d
Karina Turchyna <kari.tu99@gmail.com>
2025-01-29 22:36:22 +0200

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,194 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -36
+          },
+          "rawMisMatchPercentage": 26.47058823529412,
+          "misMatchPercentage": "26.47",
+          "analysisTime": 14
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_Entire_document.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--0.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--0.png",
+        "selector": ".stars--0",
+        "fileName": "stars--0.png",
+        "label": "stars--0",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 22.7,
+          "misMatchPercentage": "22.70",
+          "analysisTime": 4
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--0.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--1.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--1.png",
+        "selector": ".stars--1",
+        "fileName": "stars--1.png",
+        "label": "stars--1",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 27.450000000000003,
+          "misMatchPercentage": "27.45",
+          "analysisTime": 3
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--1.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--2.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--2.png",
+        "selector": ".stars--2",
+        "fileName": "stars--2.png",
+        "label": "stars--2",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 31.4,
+          "misMatchPercentage": "31.40",
+          "analysisTime": 6
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--2.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--3.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--3.png",
+        "selector": ".stars--3",
+        "fileName": "stars--3.png",
+        "label": "stars--3",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 34.150000000000006,
+          "misMatchPercentage": "34.15",
+          "analysisTime": 2
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--3.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--4.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--4.png",
+        "selector": ".stars--4",
+        "fileName": "stars--4.png",
+        "label": "stars--4",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 35.8,
+          "misMatchPercentage": "35.80",
+          "analysisTime": 2
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--4.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\stars--5.png",
+        "test": "..\\bitmaps_test\\20250129-222055\\stars--5.png",
+        "selector": ".stars--5",
+        "fileName": "stars--5.png",
+        "label": "stars--5",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stars/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 16,
+            "height": -4
+          },
+          "rawMisMatchPercentage": 36.15,
+          "misMatchPercentage": "36.15",
+          "analysisTime": 2
+        },
+        "diffImage": "..\\bitmaps_test\\20250129-222055\\failed_diff_stars--5.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
